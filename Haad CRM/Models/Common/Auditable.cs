26ed29d31de0b5f04a7aade3a4b0a55b4fa568f3
Faxrullo <faxrullofayzullayev@gmail.com>
2024-03-03 18:21:51 +0500

    changed

--- Haad CRM/Models/Common/Auditable.cs
+++ Haad CRM/Models/Common/Auditable.cs
@@ -4,6 +4,7 @@ public abstract class Auditable
 {
     public long Id { get; set; }
     public bool IsDeleted { get; set; }
+  
     public DateTime CreatAt { get; set; }  = DateTime.UtcNow;
     public DateTime UpdateAt { get; set; }
     public DateTime DeletedAd { get; set; }
