75745afd41847779be6497fc494f88d59c29092c
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-03-03 16:09:05 +0500

    a

--- Haad CRM/Interfaces/ICourseService.cs
+++ Haad CRM/Interfaces/ICourseService.cs
@@ -5,8 +5,12 @@ namespace Haad_CRM.Interfaces;
 public interface ICourseService
 {
     Task<CourseViewModel> CreateAsync(CourseCreation course);
+   
     Task<CourseViewModel> UpdateAsync(CourseUpdate course,long id);
+    
     Task<bool> DeleteAsync(long id);
+    
     Task<CourseViewModel> GetByIdAsync(long id);
+    
     Task<List<CourseViewModel>> GetAllAsync();
 }
