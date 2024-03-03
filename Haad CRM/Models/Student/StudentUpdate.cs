3c611dfba6fa12cc9077f3f5572079418f5e52fa
Faxrullo <faxrullofayzullayev@gmail.com>
2024-03-03 17:57:16 +0500

    added Ui

--- Haad CRM/Models/Student/StudentUpdate.cs
+++ Haad CRM/Models/Student/StudentUpdate.cs
@@ -11,5 +11,6 @@ public class StudentUpdate
     public string Email { get; set; }
     public string Phone { get; set; }
     public string Address { get; set; }
-   
+    public string Password { get; set; }
+
 }
