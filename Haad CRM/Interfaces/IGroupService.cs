167503522aba11f66e4eb312a49b4bf28d37e694
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-03-03 16:09:59 +0500

    as

---
+++ Haad CRM/Interfaces/IGroupService.cs
@@ -0,0 +1,17 @@
+﻿using Haad_CRM.Models.Group;
+using Haad_CRM.Models.Student;
+
+namespace Haad_CRM.Interfaces;
+
+public interface IGroupService
+{
+    Task<GroupViewModel> CreateAsync(GroupCreation group);
+
+    Task<bool> DeleteAsync(long id);
+
+    Task<GroupViewModel> GetByIdAsync(long id);
+
+    Task<GroupViewModel> UpdateAsync(GroupCreation group, long id);
+
+    Task<List<GroupViewModel>> GetAllAsync();
+}
---
+++ Haad CRM/Interfaces/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using Haad_CRM.Models.New;
+using Haad_CRM.Models.Student;
+
+namespace Haad_CRM.Interfaces;
+
+public interface INewsService
+{
+    Task<NewsViewModel> CreateAsync(NewsCreation news);
+
+    Task<bool> DeleteAsync(long id);
+
+    Task<NewsViewModel> GetByIdAsync(long id);
+
+    Task<NewsViewModel> UpdateAsync(NewsUpdate news, long id);
+
+    Task<List<NewsViewModel>> GetAllAsync();
+}
